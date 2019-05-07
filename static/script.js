d23029e928188024a6eea7a24f3fcd7824f805ac
PMuiruri <peninah.m.muiruri@gmail.com>
2019-05-07 13:12:43 +0300

    adding ajax call to post data to api

--- static/script.js
+++ static/script.js
@@ -38,28 +38,47 @@ function closeSlideMenu(){
   document.getElementById('side-menu').style.width = '0';
   document.getElementById('main').style.marginLeft = '0';
 }
-// TODO: scroll two different divs
-// function OnScroll(div) {
-//     var d1 = document.getElementById("image-scroll");
-//     d1.scrollTop = div.scrollTop;
-// }
+TODO: scroll two different divs
+function OnScroll(div) {
+
+    var images = document.getElementById("image-scroll");
+    images.scrollTop = div.scrollTop;
+      div.scrollIntoView();
+}
 // TODO: Submit form data
 function submitForm(){
-  document.getElementById('result').innerHTML="";
-  var selected = document.getElementById('sizes');
-  if(selected == null){
-    console.log("Please select a size");
-  }
-  else{
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST",'http://localhost/formTest.php', true);
+  xhr.setRequestHeader('Content-type', 'application/json')
+  xhr.onload = function (){
+    document.getElementById('result').innerHTML="";
+    var selected = document.getElementById('sizes');
     var size = selected.options[selected.selectedIndex].value;
     var pieces = document.getElementById("pieces").value;
+    var toSend ={
+      size: size,
+      pieces: pieces
+    };
 
-    if(pieces == 0){
-      return document.getElementById('result').innerHTML="Please select number of pieces"
-    } else{
-      return document.getElementById('result').innerHTML=`You chose ${pieces} pieces of size ${size}`;
-    }
+    var data = JSON.stringify(toSend);
+  }
+  xhr.send(data);
 
+  // document.getElementById('result').innerHTML="";
+  // var selected = document.getElementById('sizes');
+  // if(selected == null){
+  //   console.log("Please select a size");
+  // }
+  // else{
+  //   var size = selected.options[selected.selectedIndex].value;
+  //   var pieces = document.getElementById("pieces").value;
+  //
+  //   if(pieces == 0){
+  //     return document.getElementById('result').innerHTML="Please select number of pieces"
+  //   } else{
+  //     return document.getElementById('result').innerHTML=`You chose ${pieces} pieces of size ${size}`;
+  //   }
+  //
 
   }
 
